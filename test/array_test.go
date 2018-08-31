74834d8ce15659bb009fdd1f7ec480f0c9ad14ca
Zhenyue_Hong <zhenyue_hong@mtelnet.com>
2018-08-31 18:52:13 +0800

    性能测试

--- test/array_test.go
+++ test/array_test.go
@@ -23,11 +23,23 @@ func TestSubStr(t *testing.T) {
 		{"说我可以考虑下", 8},
 		{"一二三二一", 3},
 	}
-
 	for _, tt := range test {
 		actual := lengthOfNonRepeatingSubStr(tt.s)
 		if actual != tt.ans {
 			t.Errorf("got %d for intput %s; expected %d", actual, tt.s, tt.ans)
+		}
+	}
+}
+
+//性能测试
+func BenchmarkSubstr(b *testing.B) {
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	ans := 8
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("got %d for intput %s; expected %d",
+				actual, s, ans)
 		}
 	}
 }
